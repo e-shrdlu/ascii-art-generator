6af3f67648256409a2a27578b051d17002fa44eb
lord cavendish <>
2021-02-27 13:48:43 -0600

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
 # ascii-art-generator
 generates ascii art from pictures, videos, webcam feed, or youtube videos
+
+
+how it works:
+short version: image is shrunk, then each pixel is converted to a character based on its brightness (ex ' '=brightness 0, 'X'=brightness 100, '@'=brightness 255, etc)
+longer version: videos will be split into frames with openCV, and each frame is converted to a PIL image.
+PIL image is then resized to y=vertical_size, keeping the orignal aspect ration, then x is scaled by horizontal_scale to roughly match the original aspect ratio when converted to ascii.
+Each pixel in the resized image is itererated through, and brightness is determined by (r+g+b)/3, which is then scaled down to an integer from 0-48, and that location in char_list defines the character which will be used to represent that pixel.
+
+char_list was generated using char_brightness_rating.py, which prints out each character enough to fill terminal window, screenshots, calculates average brightness, and sorts characters based on calculated brightness
+
+youtube videos are downloaded with pytube https://github.com/pytube/pytube
+
+
+usage:
+run ascci_art_generator.py. you should see the following
+
+a) saved video
+b) saved picture
+c) screenshot
+d) webcam
+e) youtube video
+f) read saved ascii art
+g) settings
+h) quit
+
+enter a letter to choose what you want to do. The may then prompt you for input if your choice requires it. For a, b, and f, the option required is 'path'. this is where on your disc the file you want to display is.
+for e, the option required is 'url'. this is the url of the youtube video
+for d, the option required is 'webcam id'. this is the id of the webcam you want to use, which will usually be 0
+
+description of each option:
+a) saved video
+uses openCV to convert each frame of an mp4 video to ascii, and displays that on the terminal
+input: path=location of video on disc
+
+b) saved picture
+uses PIL to open an image, and prints out it's ascii art
+input: path=location of picture on disc
+
+c) screenshot
+uses PIL.ImageGrab, pyautogui, and keyboard to capture a screenshot. to use this option, move your mouse to the top left corner of what you want to caputre, press shift, move mouse to bottom right corner, pres shift again, and the program will display this in ascii art
+
+d) webcam
+uses openCV to get live webcam feed
+input: webcam id=which webcam to use, starting from 0
+
+e) youtube video
+uses pytube to download video to yt_video_path (changeable in settings menu), calls play_ascii_video, then deletes the saved file
+input: url=url of youtube video to play
+
+f) read saved ascii art
+reads and displays a text file containing ascii art generated from this program. to save ascii art, see settings menu
+input: path=location of text file
+
+g) settings
+allows user to change settings. you can change the following variables:
+float horizontal_scale -- default 2, how many characters wide each pixel should be
+int vertical_size -- default 75, how many lines the generated ascii art should be
+str yt_video_path -- default None, where to temporarily save youtube videos (must be set to use option e)
+int frames_to_play -- default 0, how many frames of a video to display before returning to menu. if 0, displays all
+int mirrored -- default 0, (0 or 1) if image should be flipped when converted. most useful with webcam feed
+str save_ascii_art_in -- default None, text file to save each ascii image in.
+
+h) quit
+pretty self explainitory
+
+
+dependencies:
+requred:
+    PIL
+    cv2
+needed for certain features:
+    pytube (for youtube feature)
+    keyboard (for screenshot feature)
+    pyautogui (for screenshot feature)
---
+++ ascii_art_generator.py
@@ -0,0 +1,233 @@
+from PIL import Image, ImageGrab
+import time
+import cv2
+import sys
+import os
+
+
+def print_large_block(text):
+    global save_ascii_art_in
+    print('',end='',flush=True)
+    sys.stdout.flush()
+    print('\n',end=text, flush=True)
+    if save_ascii_art_in:
+        with open(save_ascii_art_in, 'a') as f:
+            f.write(text+'\n')
+
+
+def get_ascii_from_image(im):
+    global char_list, mirrored
+    lines=[]
+    x_range = range(im.size[0])[::-1] if mirrored else range(im.size[0])
+    for y in range(im.size[1]):
+        line=[]
+        for x in x_range:
+            pix = sum(im.getpixel((x,y)))/3
+            char_list_pos = int((len(char_list)-1)*pix/255)
+            line.append(char_list[char_list_pos])
+        lines.append(''.join(line))
+    return '\n'.join(lines)
+
+
+def get_video_frms(path, format="cv2"):
+    cap = cv2.VideoCapture(path)#,cv2.CAP_DSHOW)
+    # print(cap,cap.isOpened())
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret == True:
+            if format=='PIL':
+                yield cv2_to_PIL(frame)
+            else:
+                yield frame
+        else: # end of file
+            cap.release()
+            cv2.destroyAllWindows()
+            break
+
+
+def cv2_to_PIL(frame):
+    color_mode_converted = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    # cv2.imshow('image',frame)
+    pil_im = Image.fromarray(color_mode_converted)
+    return pil_im
+
+
+def show_screenshot():
+    # imported inside function so as to not require install for everything else
+    failed_import = False
+    try:
+        import pyautogui
+    except ModuleNotFoundError:
+        print('you must install pyautogui for this feature. try running "python -m pip install pyautogui"')
+        failed_import=True
+    try:
+        import keyboard
+    except ModuleNotFoundError:
+        print('you must install keybord module for this feature. try running "python -m pip install keyboard"')
+        failed_import=True
+    if failed_import:
+        return -1
+
+    print('press shift @ top left corner')
+    while not keyboard.is_pressed('shift'):
+        pass
+    first_pos = pyautogui.position()
+    print('got first corner, press shift @ bottom right corner')
+    time.sleep(1)
+    while not keyboard.is_pressed('shift'):
+        pass
+    second_pos = pyautogui.position()
+    screenshot = ImageGrab.grab(bbox=(first_pos[0],first_pos[1],second_pos[0],second_pos[1]))
+    print('done')
+    print_ascii_from_im(screenshot)
+
+
+def print_ascii_from_im(im):
+    global horizontal_size, vertical_size, save_ascii_art_in
+    horizontal_size = int(vertical_size*horizontal_scale*im.size[0]/im.size[1])
+    im=im.resize((horizontal_size,vertical_size))
+    ascii_art = get_ascii_from_image(im, False)+'\n'
+    print_large_block(ascii_art)
+
+
+def play_ascii_video(path):
+    global frames_to_play
+    i=0
+    for im in get_video_frms(path, 'PIL'):
+        print_ascii_from_im(im)
+        i+=1
+        if i > frames_to_play and frames_to_play != 0:
+            break
+
+
+def download_yt_video(url, path):
+    try:
+        from pytube import YouTube # imported inside function so as to not require install for everything else
+    except ModuleNotFoundError:
+        print('you must install pytube for this feature. try running "python -m pip install git+https://github.com/pytube/pytube"')
+        return -1
+
+    yt = YouTube(url)
+    yt.streams.first().download(path)
+    path = os.path.join(path,yt.title+'.mp4')
+    return path
+
+
+def play_webcam(num):
+    play_ascii_video(int(num))
+
+
+def show_saved_picture(path):
+    im = Image.open(path)
+    print_ascii_from_im(im)
+
+
+def play_youtube_video(url):
+    global yt_video_path
+    if not os.path.exists(yt_video_path):
+        os.mkdir(yt_video_path)
+    video_path = download_yt_video(url, path=yt_video_path)
+    play_ascii_video(video_path)
+    os.remove(video_path)
+
+
+def read_ascii_art_file(path):
+    with open(path,'r') as f:
+        txt = []
+        for line in f:
+            if len(line) <= 1:
+                print_large_block(''.join(txt))
+                txt=[]
+                time.sleep(0.025)
+            else:
+                txt.append(line)
+
+
+def change_settings():
+    global char_list,horizontal_scale,vertical_size,yt_video_path,frames_to_play,mirrored,save_ascii_art_in
+    while 1:
+        print('syntax: [var]=[value], or type "back" to go back to main menu')
+        print(f"""
+        float horizontal_scale={horizontal_scale}
+        int vertical_size={vertical_size}
+        str yt_video_path={yt_video_path}
+        int frames_to_play={frames_to_play}
+        int mirrored={mirrored}
+        str save_ascii_art_in={save_ascii_art_in}""")
+        user_input = {}
+        user_input['raw'] = input('>> ')
+        if user_input['raw'] == 'back':
+            break
+
+        # parses input. stores variable to change in user_input['var'], and value to change it to in user_input['val']
+        user_input['var'] = ''
+        user_input['val'] = ''
+        i = 'var'
+        for char in user_input['raw']:
+            if char == '=' and i == 'var':
+                i = 'val'
+            else:
+                user_input[i]+=char
+        print(user_input)
+        # change value. theres gotta be a better way to do this
+        if user_input['var'] in 'horizontal_scale':
+            horizontal_size = float(user_input['val'])
+        elif user_input['var'] in 'vertical_size':
+            vertical_size = int(user_input['val'])
+        elif user_input['var'] in 'yt_video_path':
+            yt_video_path = user_input['val']
+        elif user_input['var'] in 'frames_to_play':
+            frames_to_play = int(user_input['val'])
+        elif user_input['var'] in 'mirrored':
+            mirrored = int(user_input['val'])
+        elif user_input['var'] in 'save_ascii_art_in':
+            save_ascii_art_in = user_input['val']
+
+
+def stop_execution():
+    global go
+    go = False
+
+
+def ui():
+    global go
+    go = True
+    # options, letter is how you select the option, first item is name, second is options required, third is func
+    choices = {'a':['saved video','path',play_ascii_video],'b':['saved picture','path',show_saved_picture],'c':['screenshot','',show_screenshot],'d':['webcam','webcam id (probably 0)',play_webcam],'e':['youtube video', 'url', play_youtube_video], 'f':['read saved ascii art', 'path',read_ascii_art_file], 'g':['settings', '',change_settings], 'h':['quit','', stop_execution]}
+    while go:
+        print('\n'.join([opt+') '+choices[opt][0] for opt in choices.keys()])) # prints menu
+        try:
+            # gets user choice for func to exec
+            choice=''
+            while choice not in choices.keys():
+                choice=input('> ')
+
+            # gets values to pass to chosen func
+            if choices[choice][1]:
+                print('values needed:',choices[choice][1])
+                value = input('>> ')
+                choices[choice][2](value) # calls chosen function with value given
+            else:
+                choices[choice][2]() # calls chosen function if doesnt need value
+
+        except Exception as e:
+            print(e,'\nsomething broke bro idk') # probably not best practice but idk what else to do
+
+
+def main(): # do I even need a main function? why do i have this?
+    ui()
+
+
+char_list=[' ','`','.','-',"'",',',':','_',';','"','~','*','!','+','<','7','r','/','i','^','v','l','?','t','}','j','C','x','2','S','V','y','E','O','X','G','q', 'N','0','$','b','#','D','8','%','M','W','@','&']
+horizontal_scale = 2
+vertical_size = 75
+yt_video_path='D:\\tmp\\ytvideos'
+frames_to_play = 0
+mirrored = 0
+save_ascii_art_in = '' # if not blank, will store each frame in this text file
+
+go = True
+
+
+if __name__=='__main__':
+    main()
